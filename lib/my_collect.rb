df534cdc391f48f96678cd76fb6c351eb1757038
JUAN M RESTREPO <tinto922@gmail.com>
2020-03-26 12:17:02 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+def my_collect(array)
+  i = 0 
+  new_array = []
+  
+  while array.length > i 
+    new_array << yield(array[i])
+    
+    i = i + 1
+  end 
+  new_array
+end 
 
 
+my_collect(array) do |name|
+  name.split(" ").first
+end

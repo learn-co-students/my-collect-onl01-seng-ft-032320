a86ad774aa1534a40e48829d0048d6d5ce630eb4
edmundo papi <edmundopierre@yahoo.com>
2020-03-27 21:22:37 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
 
+def my_collect(array)
+    collection =[]
+    counter = 0
+    while counter < array.length
 
+      collection << yield(array[counter])
+       counter += 1
+    end
+
+     return collection
+end

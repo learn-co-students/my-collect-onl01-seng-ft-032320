11c206863248e77e4642179000d224025089f683
Nicholas Keller <nic.keller328@gmail.com>
2020-03-27 18:35:07 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+def my_collect(list)
+  
+  if block_given?
+    i = 0
+    
+    collection = []
+    
+    while i < list.length
+    new_item = yield list[i]
+    collection << new_item
+    i += 1
+    end
 
+  else
+  end
+  collection
+end
 
